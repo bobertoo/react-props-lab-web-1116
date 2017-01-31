05de04c80c5c6a561a87f629ef2decec4218ded9
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-31 15:53:00 -0500

    spicesheep

---
+++ components/Spaceship.js
@@ -0,0 +1,19 @@
+const React = require('react')
+
+class Spaceship extends React.Component {
+  render() {
+    return ( <div>
+      <h1>{this.props.name}</h1>
+      <h2>{this.props.speed}</h2>
+      <h2>{this.props.colors}</h2>
+    </div>
+  )
+  }
+}
+
+Spaceship.defaultProps = {
+  hasRockets: false,
+  colors: ['black', 'red']
+}
+
+module.exports = Spaceship
